b254012aa2e459faa36afd129b6ffc7d6faa204b
Create or Update Pull Request Action <create-or-update-pull-request@users.noreply.github.com>
2025-04-09 09:06:44 +0000

    Horreum client updated

--- pkg/raw_client/api/config_datastore_request_builder.go
+++ pkg/raw_client/api/config_datastore_request_builder.go
@@ -11,6 +11,20 @@ import (
 type ConfigDatastoreRequestBuilder struct {
     i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.BaseRequestBuilder
 }
+// ConfigDatastoreRequestBuilderGetQueryParameters obtain list of configured datastores for particular team
+type ConfigDatastoreRequestBuilderGetQueryParameters struct {
+    // name of the team to search for defined datastores
+    Team *string `uriparametername:"team"`
+}
+// ConfigDatastoreRequestBuilderGetRequestConfiguration configuration for the request such as headers, query parameters, and middleware options.
+type ConfigDatastoreRequestBuilderGetRequestConfiguration struct {
+    // Request headers
+    Headers *i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.RequestHeaders
+    // Request options
+    Options []i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.RequestOption
+    // Request query parameters
+    QueryParameters *ConfigDatastoreRequestBuilderGetQueryParameters
+}
 // ConfigDatastoreRequestBuilderPostRequestConfiguration configuration for the request such as headers, query parameters, and middleware options.
 type ConfigDatastoreRequestBuilderPostRequestConfiguration struct {
     // Request headers
@@ -27,31 +41,31 @@ type ConfigDatastoreRequestBuilderPutRequestConfiguration struct {
 }
 // ById gets an item from the github.com/hyperfoil/horreum-client-golang/pkg/raw_client.api.config.datastore.item collection
 // Deprecated: This indexer is deprecated and will be removed in the next major version. Use the one with the typed parameter instead.
-// returns a *ConfigDatastoreItemRequestBuilder when successful
-func (m *ConfigDatastoreRequestBuilder) ById(id string)(*ConfigDatastoreItemRequestBuilder) {
+// returns a *ConfigDatastoreDatastoreItemRequestBuilder when successful
+func (m *ConfigDatastoreRequestBuilder) ById(id string)(*ConfigDatastoreDatastoreItemRequestBuilder) {
     urlTplParams := make(map[string]string)
     for idx, item := range m.BaseRequestBuilder.PathParameters {
         urlTplParams[idx] = item
     }
     if id != "" {
-        urlTplParams["%2Did"] = id
+        urlTplParams["id"] = id
     }
-    return NewConfigDatastoreItemRequestBuilderInternal(urlTplParams, m.BaseRequestBuilder.RequestAdapter)
+    return NewConfigDatastoreDatastoreItemRequestBuilderInternal(urlTplParams, m.BaseRequestBuilder.RequestAdapter)
 }
 // ByIdInteger gets an item from the github.com/hyperfoil/horreum-client-golang/pkg/raw_client.api.config.datastore.item collection
-// returns a *ConfigDatastoreItemRequestBuilder when successful
-func (m *ConfigDatastoreRequestBuilder) ByIdInteger(id int32)(*ConfigDatastoreItemRequestBuilder) {
+// returns a *ConfigDatastoreDatastoreItemRequestBuilder when successful
+func (m *ConfigDatastoreRequestBuilder) ByIdInteger(id int32)(*ConfigDatastoreDatastoreItemRequestBuilder) {
     urlTplParams := make(map[string]string)
     for idx, item := range m.BaseRequestBuilder.PathParameters {
         urlTplParams[idx] = item
     }
-    urlTplParams["%2Did"] = i53ac87e8cb3cc9276228f74d38694a208cacb99bb8ceb705eeae99fb88d4d274.FormatInt(int64(id), 10)
-    return NewConfigDatastoreItemRequestBuilderInternal(urlTplParams, m.BaseRequestBuilder.RequestAdapter)
+    urlTplParams["id"] = i53ac87e8cb3cc9276228f74d38694a208cacb99bb8ceb705eeae99fb88d4d274.FormatInt(int64(id), 10)
+    return NewConfigDatastoreDatastoreItemRequestBuilderInternal(urlTplParams, m.BaseRequestBuilder.RequestAdapter)
 }
 // NewConfigDatastoreRequestBuilderInternal instantiates a new ConfigDatastoreRequestBuilder and sets the default values.
 func NewConfigDatastoreRequestBuilderInternal(pathParameters map[string]string, requestAdapter i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.RequestAdapter)(*ConfigDatastoreRequestBuilder) {
     m := &ConfigDatastoreRequestBuilder{
-        BaseRequestBuilder: *i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.NewBaseRequestBuilder(requestAdapter, "{+baseurl}/api/config/datastore", pathParameters),
+        BaseRequestBuilder: *i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.NewBaseRequestBuilder(requestAdapter, "{+baseurl}/api/config/datastore{?team*}", pathParameters),
     }
     return m
 }
@@ -60,6 +74,25 @@ func NewConfigDatastoreRequestBuilder(rawUrl string, requestAdapter i2ae4187f7da
     urlParams := make(map[string]string)
     urlParams["request-raw-url"] = rawUrl
     return NewConfigDatastoreRequestBuilderInternal(urlParams, requestAdapter)
+}
+// Get obtain list of configured datastores for particular team
+// returns a []Datastoreable when successful
+func (m *ConfigDatastoreRequestBuilder) Get(ctx context.Context, requestConfiguration *ConfigDatastoreRequestBuilderGetRequestConfiguration)([]i24479a9d05b05b7c1efaeda9ae24aee51c8acc6f59ee3190ae7f0941a410c8a1.Datastoreable, error) {
+    requestInfo, err := m.ToGetRequestInformation(ctx, requestConfiguration);
+    if err != nil {
+        return nil, err
+    }
+    res, err := m.BaseRequestBuilder.RequestAdapter.SendCollection(ctx, requestInfo, i24479a9d05b05b7c1efaeda9ae24aee51c8acc6f59ee3190ae7f0941a410c8a1.CreateDatastoreFromDiscriminatorValue, nil)
+    if err != nil {
+        return nil, err
+    }
+    val := make([]i24479a9d05b05b7c1efaeda9ae24aee51c8acc6f59ee3190ae7f0941a410c8a1.Datastoreable, len(res))
+    for i, v := range res {
+        if v != nil {
+            val[i] = v.(i24479a9d05b05b7c1efaeda9ae24aee51c8acc6f59ee3190ae7f0941a410c8a1.Datastoreable)
+        }
+    }
+    return val, nil
 }
 // Post create a new Datastore
 // returns a *int32 when successful
@@ -92,6 +125,20 @@ func (m *ConfigDatastoreRequestBuilder) Put(ctx context.Context, body i24479a9d0
         return nil, nil
     }
     return res.(*int32), nil
+}
+// ToGetRequestInformation obtain list of configured datastores for particular team
+// returns a *RequestInformation when successful
+func (m *ConfigDatastoreRequestBuilder) ToGetRequestInformation(ctx context.Context, requestConfiguration *ConfigDatastoreRequestBuilderGetRequestConfiguration)(*i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.RequestInformation, error) {
+    requestInfo := i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.NewRequestInformationWithMethodAndUrlTemplateAndPathParameters(i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.GET, m.BaseRequestBuilder.UrlTemplate, m.BaseRequestBuilder.PathParameters)
+    if requestConfiguration != nil {
+        if requestConfiguration.QueryParameters != nil {
+            requestInfo.AddQueryParameters(*(requestConfiguration.QueryParameters))
+        }
+        requestInfo.Headers.AddAll(requestConfiguration.Headers)
+        requestInfo.AddRequestOptions(requestConfiguration.Options)
+    }
+    requestInfo.Headers.TryAdd("Accept", "application/json")
+    return requestInfo, nil
 }
 // ToPostRequestInformation create a new Datastore
 // returns a *RequestInformation when successful
